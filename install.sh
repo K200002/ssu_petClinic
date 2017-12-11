946c23fe8819591da922cef337737f09b001c4fe
kris <krismail95@gmail.com>
2017-12-11 22:20:10 +0300

    fix

--- install.sh
+++ install.sh
@@ -8,16 +8,34 @@ sudo service jenkins stop
 sudo cp -rf $PYTH_VBOX/ssu_petClinic/jenkins /etc/default
 sudo chown -R jenkins:jenkins /etc/default/jenkins
 
-sudo cp -rf $PYTH_VBOX/ssu_petClinic/users/* $HOME_JENKINS/users/admin
-sudo chown -R jenkins:jenkins $HOME_JENKINS/users/*
-sudo chown -R jenkins:jenkins $HOME_JENKINS/users
-
 sudo cp -rf $PYTH_VBOX/ssu_petClinic/config/* $HOME_JENKINS
 sudo chown -R jenkins:jenkins $HOME_JENKINS/*
 
 sudo cp -rp $PYTH_VBOX/ssu_petClinic/jobs/* $HOME_JENKINS/jobs
 sudo chown -R jenkins:jenkins $HOME_JENKINS/jobs/*
 sudo chown -R jenkins:jenkins $HOME_JENKINS/jobs
+
+
+
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/script-security.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/command-launcher.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/bouncycastle-api.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/structs.hpi
+#wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/
+#wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/credentials.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/apache-httpcomponents-client-4-api.hpi
+#wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/
+#wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/
+#wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/junit.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/windows-slaves.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/display-url-api.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/mailer.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/matrix-auth.hpi
+#wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/matrix-project.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/jsch.hpi
 
 wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/git-client.hpi
 wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/git.hpi
