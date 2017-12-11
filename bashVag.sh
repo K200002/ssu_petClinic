95762cda3f67045570a0be70c8e1569770f863d9
kris <krismail95@gmail.com>
2017-12-11 20:57:18 +0300

    fix

--- bashVag.sh
+++ bashVag.sh
@@ -1,47 +1,21 @@
 #! /bin/bash
-sudo apt-get -y install wget
+echo "!!!!!!!!!Install jre"
 sudo apt-get -y install openjdk-8-jre
+echo "!!!!!!!!!Install jdk"
+sudo apt-get -y install openjdk-8-jdk
+echo "!!!!!!!!!Install git"
 sudo apt-get -y install git
-sudo apt-get -y install maven
+
+echo "!!!!!!!!Install jenkins"
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get -y install jenkins
 
-sudo service jenkins stop
-git clone https://github.com/krismal95/ssu_petClinic.git
+echo "!!!!!!!Install maven"
+sudo apt-get -y install maven
 
-HOME_JENKINS=/var/lib/jenkins
-PYTH_VBOX=`pwd`
-
-sudo cp -rf $PYTH_VBOX/ssu_petClinic/jenkins /etc/default
-sudo chown -R jenkins:jenkins /etc/default/jenkins
-
-sudo cp -rf $PYTH_VBOX/ssu_petClinic/users/* $HOME_JENKINS/users/admin
-sudo chown -R jenkins:jenkins $HOME_JENKINS/users/*
-sudo chown -R jenkins:jenkins $HOME_JENKINS/users
-
-sudo cp -rf $PYTH_VBOX/ssu_petClinic/config/* $HOME_JENKINS
-sudo chown -R jenkins:jenkins $HOME_JENKINS/*
-
-sudo cp -rp $PYTH_VBOX/ssu_petClinic/jobs/* $HOME_JENKINS/jobs
-sudo chown -R jenkins:jenkins $HOME_JENKINS/jobs/*
-sudo chown -R jenkins:jenkins $HOME_JENKINS/jobs
-
-wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/git-client.hpi
-wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/git.hpi
-wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/JDK_Parameter_Plugin.hpi
-sudo chown -R jenkins:jenkins $HOME_JENKINS/plugins/*
-
-sudo iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
-sudo iptables -I INPUT 1 -p tcp --dport 8090 -j ACCEPT
-
-sudo service jenkins start
-
-
-
-
-
+./install.sh
 
 
 
---
+++ install.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+echo "Install all"
+HOME_JENKINS=/var/lib/jenkins
+PYTH_VBOX=`pwd`
+
+git clone https://github.com/krismal95/ssu_petClinic.git
+sudo service jenkins stop
+
+sudo cp -rf $PYTH_VBOX/ssu_petClinic/jenkins /etc/default
+sudo chown -R jenkins:jenkins /etc/default/jenkins
+
+sudo cp -rf $PYTH_VBOX/ssu_petClinic/users/* $HOME_JENKINS/users/admin
+sudo chown -R jenkins:jenkins $HOME_JENKINS/users/*
+sudo chown -R jenkins:jenkins $HOME_JENKINS/users
+
+sudo cp -rf $PYTH_VBOX/ssu_petClinic/config/* $HOME_JENKINS
+sudo chown -R jenkins:jenkins $HOME_JENKINS/*
+
+sudo cp -rp $PYTH_VBOX/ssu_petClinic/jobs/* $HOME_JENKINS/jobs
+sudo chown -R jenkins:jenkins $HOME_JENKINS/jobs/*
+sudo chown -R jenkins:jenkins $HOME_JENKINS/jobs
+
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/git-client.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/git.hpi
+wget -N -P $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/JDK_Parameter_Plugin.hpi
+sudo chown -R jenkins:jenkins $HOME_JENKINS/plugins/*
+
+sudo iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
+sudo iptables -I INPUT 1 -p tcp --dport 8090 -j ACCEPT
+
+sudo service jenkins start
+
+
+
+
+
